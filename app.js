b267ff653c5dd9d581ec103ff56809511e183849
unknown <povilas.kascionis@gmail.com>
2018-10-08 12:57:53 +0300

    update connection to mongo and use unshift for comment save due to push depreaction in mongoose.

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-var express             = require("express"),
-    bodyParser          = require("body-parser"),
-    app                 = express(),
-    mongoose            = require("mongoose"),
-    passport            = require("passport"),
-    flash               = require("connect-flash"),
-    LocalStrategy       = require("passport-local"),
-    User                = require("./models/user"),
-    methodOverride      = require("method-override");
-    
-var commentRoutes       = require("./routes/comments"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    indexRoutes         = require("./routes/index");
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  mongoose = require('mongoose'),
+  passport = require('passport'),
+  flash = require('connect-flash'),
+  LocalStrategy = require('passport-local'),
+  User = require('./models/user'),
+  methodOverride = require('method-override');
 
-mongoose.connect(process.env.DATABASEURL);
+var commentRoutes = require('./routes/comments'),
+  campgroundRoutes = require('./routes/campgrounds'),
+  indexRoutes = require('./routes/index');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+mongoose.connect(
+  process.env.DATABASEURL,
+  { useMongoClient: true }
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 app.use(flash());
 
 // PASSPORT CONFIG
-app.use(require("express-session")({
-    secret: "no country for old men",
+app.use(
+  require('express-session')({
+    secret: 'no country for old men',
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -33,18 +38,18 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    res.locals.info = req.flash("info");
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  res.locals.info = req.flash('info');
+  next();
 });
 
 app.use(indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("YelpCamp online...");
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log('YelpCamp online...');
 });
--- routes/comments.js
+++ routes/comments.js
@@ -1,33 +1,33 @@
 var express = require("express");
-var router = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
 var middleware = require("../middleware");
 
-router.get("/new", middleware.isLoggedIn, function(req, res){
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
+router.get("/new", middleware.isLoggedIn, function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
             console.log(err);
         } else {
-               res.render("comments/new", {campground: campground});
+            res.render("comments/new", { campground: campground });
         }
     });
 });
 
-router.post("/", middleware.isLoggedIn, function(req, res){
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
+router.post("/", middleware.isLoggedIn, function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
             console.log(err);
         } else {
-            Comment.create(req.body.comment, function(err, comment){
-                if(err){
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
                     console.log(err);
                 } else {
                     //add username and ID to comment
                     comment.author.id = req.user._id;
                     comment.author.username = req.user.username;
                     comment.save();
-                    campground.comments.push(comment);
+                    campground.comments.unshift(comment);
                     campground.save();
                     res.redirect("/campgrounds/" + campground._id);
                 }
@@ -37,20 +37,20 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 });
 
 //COMMENT EDIT ROUTE
-router.get("/:comment_id/edit", middleware.checkCommentOwnership, function (req, res){
-    Comment.findById(req.params.comment_id, function(err, foundComment){
-        if(err){
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) {
             res.redirect("back");
         } else {
-            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
         }
     });
 });
 
 //COMMENT UPDATE ROUTE
-router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
-    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
-        if(err){
+router.put("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+        if (err) {
             res.redirct("back");
         } else {
             req.flash("success", "Comment has been updated")
@@ -60,15 +60,15 @@ router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
 });
 
 //COMMENT DELETE ROUTE
-router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
-   Comment.findByIdAndRemove(req.params.comment_id, function(err){
-       if(err){
-           console.log("whoopsie daysies");
-       } else {
-           req.flash("info", "Your comment was deleted")
-           res.redirect("/campgrounds/" + req.params.id);
-       }
-   }) ;
+router.delete("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if (err) {
+            console.log("whoopsie daysies");
+        } else {
+            req.flash("info", "Your comment was deleted")
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
 });
 
 
